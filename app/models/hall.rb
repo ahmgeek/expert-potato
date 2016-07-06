477384db71e3df75e206c908424c3df42ccce8ba
Ahmad A.Elghany <tolpa1@gmail.com>
2016-07-07 01:07:07 +0200

    Add relations and validations

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -1,2 +1,6 @@
 class Hall < ApplicationRecord
+  has_many :messages, dependent: :destroy
+  has_many :users, through: :messages
+
+  validates :name, presence: true, uniqueness: true, case_sensitive: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :messages
+  has_many :halls, through: :messages
+
+  validates :username, presence: true, uniqueness: true
 end
