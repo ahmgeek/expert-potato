1a41da59252629f3e2e7e87fb92807bc15600d95
Ahmad A.Elghany <tolpa1@gmail.com>
2016-07-09 23:57:11 +0200

    Add contrrollers and scaffold them a little

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  helper_method :current_user, :signed_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    !!current_user #negating the value to boolean
+  end
+
+  protected
+  def authenticate_user!
+    redirect_to root_path unless signed_in?
+  end
 end
