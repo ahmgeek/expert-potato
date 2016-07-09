417eca962960d46ddaa3e5b8d3285a1589087c6a
Ahmad A.Elghany <tolpa1@gmail.com>
2016-07-09 23:36:02 +0200

    Add user unit tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save new user" do
+    user = User.new(username: 'ahmgeek')
+    assert user.save
+  end
+
+  test "should not save user without username" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should not save user with an existing username" do
+    User.new(username: 'ahmgeek').save
+    assert_not User.new(username: 'ahmgeek').save
+  end
 end
