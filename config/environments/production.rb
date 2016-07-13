1ad09ad45037c589a335b2de5b8ffd45ee4e0673
Ahmad A.Elghany <tolpa1@gmail.com>
2016-07-13 03:01:48 +0200

    Add production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,11 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+
+  # You really need to move these to environment vars..
+  config.action_cable.allowed_request_origins = ['https://expert-potato.herokuapp.com', 'http://expert-potato.herokuapp.com']
+  config.web_socket_server_url = "wss://expert-potato.herokuapp.com/cable" 
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
